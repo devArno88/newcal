c68b19a4f2cdad819174d12fed992be8f9891fef
Alex Arno <a.j.f.arno@gmail.com>
2022-09-27 13:28:40 +0100

    NextAuth setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,14 @@ import { SessionProvider } from "next-auth/react"
 import "./styles.css"
 
 import type { AppProps } from "next/app"
-import type { Session } from "next-auth"
+// import type { Session } from "next-auth/react";
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps<{ session: Session }>) {
+}: AppProps<{ session: any }>) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
