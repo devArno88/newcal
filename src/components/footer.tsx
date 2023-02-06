66974111de990b2bff7db8d14f19a170aca0a01c
Alex <alex@devarno.com>
2023-02-06 20:10:21 +0000

    Improvements

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,24 +4,24 @@ import packageJSON from "package.json";
 
 export default function Footer() {
     return (
-        <footer className={styles.footer}>
+        <footer>
             <hr />
-            <ul className={styles.navItems}>
-                <li className={styles.navItem}>
+            <ul>
+                <li>
                     <a href="https://next-auth.js.org">Documentation</a>
                 </li>
-                <li className={styles.navItem}>
+                <li>
                     <a href="https://www.npmjs.com/package/next-auth">NPM</a>
                 </li>
-                <li className={styles.navItem}>
+                <li>
                     <a href="https://github.com/nextauthjs/next-auth-example">GitHub</a>
                 </li>
-                <li className={styles.navItem}>
+                <li>
                     <Link href="/policy">
                         <a>Policy</a>
                     </Link>
                 </li>
-                <li className={styles.navItem}>
+                <li>
                     <em>next-auth@{packageJSON.dependencies["next-auth"]}</em>
                 </li>
             </ul>
